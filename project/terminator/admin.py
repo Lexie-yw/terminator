Lexie-yw/terminator

commit 72126d16f9c92a20e75fd885d1bc1b072c01c1f3

Only show concepts from the relevant glossary
Validation only allows concepts from the glosary as subject_field,
broader_concept and in related_concepts. Let's not trick the user and
present incorrect ones. This is also a performance killer if there are
many concepts in the database, so best to limit it where we can.

project/terminator/admin.py
@@ -154,6 +154,28 @@ class ConceptAdmin(admin.ModelAdmin):
                                         Glossary, False)
         return qs.filter(glossary__in=inner_qs)
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        glossary_id = request.GET.get('glossary', None)
+        if not glossary_id:
+            return super(ConceptLanguageMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+        # only show concepts from this glossary
+        available_concepts = Concept.objects.filter(glossary_id=glossary_id)
+        if db_field.name in ("subject_field", "broader_concept"):
+            kwargs["queryset"] = available_concepts
+        return super(ConceptAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        glossary_id = request.GET.get('glossary', None)
+        if not glossary_id:
+            return super(ConceptLanguageMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+        # only show concepts from this glossary
+        available_concepts = Concept.objects.filter(glossary_id=glossary_id)
+        if db_field.name == "related_concepts":
+            kwargs["queryset"] = available_concepts
+        return super(ConceptAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
     def response_change(self, request, obj):
         return HttpResponseRedirect(obj.get_absolute_url())
 

commit ef286f76f98d0cc39c2704f6e7b631d7ff2c7e64

Fix super() calls
Incorrectly introduced in 72126d1.

project/terminator/admin.py
@@ -157,7 +157,7 @@ class ConceptAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         glossary_id = request.GET.get('glossary', None)
         if not glossary_id:
-            return super(ConceptLanguageMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+            return super(ConceptAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
         # only show concepts from this glossary
         available_concepts = Concept.objects.filter(glossary_id=glossary_id)
@@ -168,7 +168,7 @@ class ConceptAdmin(admin.ModelAdmin):
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         glossary_id = request.GET.get('glossary', None)
         if not glossary_id:
-            return super(ConceptLanguageMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+            return super(ConceptAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
         # only show concepts from this glossary
         available_concepts = Concept.objects.filter(glossary_id=glossary_id)